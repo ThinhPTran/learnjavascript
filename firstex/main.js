ba5b4eb59f6f9d591d5182c1574a375c9d21d9ec
Thinh P. Tran <dand@appsmiths.com>
2017-09-06 15:40:40 +0700

    test paper.js

--- firstex/main.js
+++ firstex/main.js
@@ -2,4 +2,22 @@
 $(document).ready(function () {
   'use strict';
   console.log('main.js loaded');
+  paper.install(window);
+  paper.setup(document.getElementById('mainCanvas'));
+
+  //TODO
+  var c = Shape.Circle(200, 200, 50);
+  var d = Shape.Rectangle(250, 150, 50, 50);
+
+  c.fillColor = 'green';
+  d.fillColor = 'blue';
+
+  for (var x = 25; x<400; x+=50) {
+    for (var y = 25; y<400; y+=50) {
+      c = Shape.Circle(x,y,20);
+      c.fillColor = 'pink';
+    }
+  }
+
+  paper.view.draw();
 });
